b6986d37d3885d9160af8a48252297d415889eef
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-07-23 21:55:18 -0300

    criação parcial dos arquivos java.

---
+++ src/br/com/java/connection/Connection.java
@@ -0,0 +1,7 @@
+package br.com.java.connection;
+
+public class Connection getConnection() throws Exception{
+    className.forName("");
+
+    Connection con = 
+}
--- src/br/com/java/model/Usuario.java
+++ src/br/com/java/model/Usuario.java
@@ -2,4 +2,42 @@ package br.com.java.model;
 
 public class Usuario {
 
+    private int usuarioid;
+    private String unome;
+    private String pwd;
+    private String email;
+    private String endereco;
+    private String telefone;
+
+    private boolean logado = false;
+
+    public int getUsuarioid() {
+        return usuarioid;
+    }
+
+    public String getUnome() {
+        return unome;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public boolean isLogado() {
+        return logado;
+    }
+
+    
 }
